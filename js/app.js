356fe123f66b988228bd4b56006f0a7dc7b8ca2a
a.ashrafi <a.ashrafi@cotint.ir>
2017-10-21 15:21:52 +0330

    add file app.js

---
+++ js/app.js
@@ -0,0 +1,180 @@
+$('.date-content').click(function () {
+    $('.box-c').addClass('hidden');
+    $('.click-date').addClass('hidden');
+});
+$('.date-footer').click(function () {
+    $('.box-c').addClass('hidden');
+    $('.click-date').addClass('hidden');
+});
+$('.date-piker-text').click(function () {
+    $('.box-c').addClass('hidden');
+    $('.click-date').addClass('hidden');
+});
+$('.tag-p-left').click(function () {
+    $('.box-c').addClass('hidden');
+    $('.click-date').addClass('hidden');
+});
+$('.tag-p-right').click(function () {
+    $('.box-c').addClass('hidden');
+    $('.click-date').addClass('hidden');
+});
+$('.title-content').click(function () {
+    $('.box-c').addClass('hidden');
+    $('.click-date').addClass('hidden');
+});
+$('.color-footer-date').click(function () {
+    $('.box-c').addClass('hidden');
+    $('.click-date').addClass('hidden');
+});
+$(".date-append").click(function () {
+    $('.box-c').addClass('hidden');
+    $(this).append("<div class=\"click-date\">\n" +
+        "    <div class=\"row\">\n" +
+        "    <div class=\"col-md-c col-xs-c show-p\" onclick=\"date(1)\">\n" +
+        "        <a href='#'><p>فروردین </p></a>\n" +
+        "    </div>\n" +
+        "    <div class=\"col-md-c col-xs-c show-p\" onclick=\"date(2)\">\n" +
+        "        <a href='#'><p>اردیبهشت</p></a>\n" +
+        "    </div>\n" +
+        "    <div class=\"col-md-c col-xs-c show-p\" onclick=\"date(3)\">\n" +
+        "        <a href='#'><p>خرداد</p></a>\n" +
+        "    </div>\n" +
+        "<div class=\"col-md-c col-xs-c show-p\"onclick=\"date(4)\">\n" +
+        "        <a href='#'><p>تیر</p></a>\n" +
+        "    </div>\n" +
+        "    <div class=\"col-md-c col-xs-c show-p\" onclick=\"date(5)\">\n" +
+        "        <a href='#'><p>مرداد</p></a>\n" +
+        "    </div>\n" +
+        "    <div class=\"col-md-c col-xs-c show-p\" onclick=\"date(6)\">\n" +
+        "        <a href='#'><p>شهریور</p></a>\n" +
+        "    </div>\n" +
+        "<div class=\"col-md-c col-xs-c show-p\" onclick=\"date(7)\">\n" +
+        "        <a href='#'><p>مهر</p></a>\n" +
+        "    </div>\n" +
+        "    <div class=\"col-md-c col-xs-c show-p\" onclick=\"date(8)\">\n" +
+        "        <a href='#'><p>ابان</p></a>\n" +
+        "    </div>\n" +
+        "    <div class=\"col-md-c col-xs-c show-p\" onclick=\"date(9)\" >\n" +
+        "        <a href='#'><p>اذر</p></a>\n" +
+        "    </div>\n" +
+        "<div class=\"col-md-c col-xs-c show-p\" onclick=\"date(10)\">\n" +
+        "        <a href='#'><p>دی</p></a>\n" +
+        "    </div>\n" +
+        "    <div class=\"col-md-c col-xs-c show-p\" onclick=\"date(11)\">\n" +
+        "        <a href='#'><p>بهمن</p></a>\n" +
+        "    </div>\n" +
+        "    <div class=\"col-md-c col-xs-c show-p\" onclick=\"date(12)\">\n" +
+        "        <a href='#'><p>اسفند</p></a>\n" +
+        "    </div>\n" +
+        "    </div>\n" +
+        "</div>");
+});
+$(".date-append-c").click(function () {
+    $('.click-date').addClass('hidden');
+    $(this).append(
+        "<div class=\"box-c mCustomScrollbar scroll-cotint\" data-mcs-theme=\"minimal-dark\">\n" +
+        "    <div class=\"row\">\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1391)\">\n" +
+        "            <a href='#'><p>1391</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1392)\">\n" +
+        "            <a href='#'><p>1392</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1393)\">\n" +
+        "            <a href='#'><p>1393</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1394)\">\n" +
+        "            <a href='#'><p>1394</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1395)\">\n" +
+        "            <a href='#'><p>1395</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1396)\">\n" +
+        "            <a href='#'><p>1396</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1397)\">\n" +
+        "            <a href='#'><p>1397</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1398)\">\n" +
+        "            <a href='#'><p>1398</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1399)\">\n" +
+        "            <a href='#'><p>1399</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1400)\">\n" +
+        "            <a href='#'><p>1400</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1401)\">\n" +
+        "            <a href='#'><p>1401</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1402)\">\n" +
+        "            <a href='#'><p>1402</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1403)\">\n" +
+        "            <a href='#'><p>1403</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1404)\">\n" +
+        "            <a href='#'><p>1404</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1405)\">\n" +
+        "            <a href='#'><p>1405</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1406)\">\n" +
+        "            <a href='#'><p>1406</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1407)\">\n" +
+        "            <a href='#'><p>1407</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1408)\">\n" +
+        "            <a href='#'><p>1408</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1409)\">\n" +
+        "            <a href='#'><p>1409</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1410)\">\n" +
+        "            <a href='#'><p>1410</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1411)\">\n" +
+        "            <a href='#'><p>1411</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1412)\">\n" +
+        "            <a href='#'><p>1412</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1413)\">\n" +
+        "            <a href='#'><p>1413</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1414)\">\n" +
+        "            <a href='#'><p>1414</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1415)\">\n" +
+        "            <a href='#'><p>1415</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1416)\">\n" +
+        "            <a href='#'><p>1416</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1417)\">\n" +
+        "            <a href='#'><p>1417</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1418)\">\n" +
+        "            <a href='#'><p>1418</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1419)\">\n" +
+        "            <a href='#'><p>1419</p></a>\n" +
+        "        </div>\n" +
+        "        <div class=\"cotint-date\" onclick=\"date(1420)\">\n" +
+        "            <a href='#'><p>1420</p></a>\n" +
+        "        </div>\n" +
+        "    </div>\n" +
+        "</div>");
+});
+
+(function ($) {
+    $(window).on("load", function () {
+        $(".scroll-c").mCustomScrollbar({
+            axis: "yx"
+        });
+        $(".scroll-cotint").mCustomScrollbar({
+            axis: "yx"
+        });
+    });
+})(jQuery);
